bce817b48f4562094189df1dcfe8a61746a346c7
ig0r2 <yo.iggor@gmail.com>
2024-10-16 00:38:24 +0200

    Added observer for fade in effects

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// Mobile Menu Navbar
+// MOBILE MENU NAVBAR
 const menu = document.querySelector("#mobile-menu");
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 // Toggle Mobile Menu
@@ -13,3 +13,28 @@ menu.querySelectorAll("a").forEach((e) =>
     hamburgerIcon.classList.remove("open");
   }),
 );
+
+// OBSERVER
+// Observe elements to show them when they are in the viewport
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.remove("obs-hidden");
+        setTimeout(() => {
+          entry.target.classList.remove(
+            ...Array.from(entry.target.classList).filter((className) =>
+              className.startsWith("obs-"),
+            ),
+          );
+        }, 3000);
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.3,
+  },
+);
+
+document.querySelectorAll(".obs-hidden").forEach((el) => observer.observe(el));
